ba34865a95fae42ea7ab9434696a1f7d2ee9c174
nikistos <nikistos@outlook.com>
2018-12-22 14:13:52 +0500

    commment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ChristmasTree from '../ChristmasTree/ChristmasTree';
 import styles from './style.css';
-
+//jhghh
 export default class App extends React.Component {
     render () {
         return (
