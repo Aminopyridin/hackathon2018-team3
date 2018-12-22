61deabcf4cfb69ce6a3b8883e33caee541eebf77
Your Name <you@example.com>
2018-12-22 14:25:08 +0500

    add ball

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChristmasTree from '../ChristmasTree/ChristmasTree';
 import styles from './style.css';
+import Balls from "../Balls/Balls";
 //jhghh
 export default class App extends React.Component {
     render () {
@@ -8,6 +9,7 @@ export default class App extends React.Component {
             <div className={styles.app}>
                 <div className={styles.treeWrapper}>
                     <ChristmasTree />
+                    <Balls />
                 </div>
             </div>
         );
