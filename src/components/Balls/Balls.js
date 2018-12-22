eae6c14d4cfe4a818211fc859bae396cda6843fc
Your Name <you@example.com>
2018-12-22 15:14:26 +0500

    switch balls

--- src/components/Balls/Balls.js
+++ src/components/Balls/Balls.js
@@ -5,10 +5,12 @@ import PropTypes from 'prop-types';
 export default class Balls extends React.Component {
     constructor(props){
         super(props);
+        this.state={ color: 'black'};
     }
     render() {
         const {l, t, color} = this.props;
-        return (<div className={`${styles.ball}`} style={{top: t, left: l, background: color}}/>);
+        return (<div className={`${styles.ball}`} style={{top: t, left: l, background: this.state.color}}
+                     onClick={() => { this.setState({ color: color });}}/>);
     }
 }
 
